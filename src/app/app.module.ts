74161e32985656be8e4e243971ebe695eb173db2
Edson Neri <edson.neri@correo.uia.mx>
2017-10-30 19:31:31 -0600

    add Footer, Header and Portfolio components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/portfolio/portfolio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styles: []
+})
+export class PortfolioComponent {
+
+  constructor() { }
+
+}
